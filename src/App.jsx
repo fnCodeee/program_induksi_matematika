331fab950b0f08a0f7f5a0d1ea32b6c5109e2c63
fnCodeee <faizna2213@gmail.com>
2026-04-05 00:49:05 +0700

    add (main): main page add name and NIM

--- src/App.jsx
+++ src/App.jsx
@@ -34,18 +34,24 @@ const App = () => {
 
       <main
         // style={{ opacity: loaderFinished ? 1 : 0, transition: "opacity 0.5s" }}
-        className="h-screen w-full flex p-6 gap-6 flex-col justify-between md:flex-row"
+        className="h-screen w-full flex flex-col p-6 justify-between"
       >
-        <Card
-          typeOfMath="Bilangan Ganjil"
-          hasil={hasilGanjil}
-          input={funcGanjil}
-        />
-        <Card
-          typeOfMath="Bilangan Kuadrat"
-          hasil={hasilKuadrat}
-          input={funcKuadrat}
-        />
+        <div className="w-full flex flex-col md:flex-row justify-between">
+          <p>Nama : Faiz Atha Aghitsna</p>
+          <p>NIM : 4112755201250113</p>
+        </div>
+        <div className="h-full w-full flex py-4 gap-6 flex-col justify-between md:flex-row">
+          <Card
+            typeOfMath="Bilangan Ganjil"
+            hasil={hasilGanjil}
+            input={funcGanjil}
+          />
+          <Card
+            typeOfMath="Bilangan Kuadrat"
+            hasil={hasilKuadrat}
+            input={funcKuadrat}
+          />
+        </div>
       </main>
     </>
   );
